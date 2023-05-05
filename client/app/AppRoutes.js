3c1e74ef697f8c75a95b1bf8af1ae3e43dd771e5
Steffan Cross <steffancross@berkeley.edu>
2023-05-05 08:43:28 -0700

    package.json name updated

--- client/app/AppRoutes.js
+++ client/app/AppRoutes.js
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
-import AuthForm from '../features/auth/AuthForm';
-import Home from '../features/home/Home';
-import { me } from './store';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+import AuthForm from "../features/auth/AuthForm";
+import Home from "../features/home/Home";
+import Chat from "../features/chat/Chat";
+import { me } from "./store";
 
 /**
  * COMPONENT
@@ -23,6 +24,7 @@ const AppRoutes = () => {
         <Routes>
           <Route path="/*" element={<Home />} />
           <Route to="/home" element={<Home />} />
+          <Route path="/chat" element={<Chat />} />
         </Routes>
       ) : (
         <Routes>
@@ -38,6 +40,7 @@ const AppRoutes = () => {
             path="/signup"
             element={<AuthForm name="signup" displayName="Sign Up" />}
           />
+          <Route path="/chat" element={<Chat />} />
         </Routes>
       )}
     </div>

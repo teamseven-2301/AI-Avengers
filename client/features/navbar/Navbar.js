0e880c1c56c9e5eeac09344fd3d0513f41b55510
Steffan Cross <steffancross@berkeley.edu>
2023-05-08 14:15:58 -0700

    styling for home, about, and guide 80% done

--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -1,42 +1,39 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
-import { logout } from '../../app/store';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { logout } from "../../app/store";
 
 const Navbar = () => {
-  const isLoggedIn = useSelector(state => !!state.auth.me.id);
+  const isLoggedIn = useSelector((state) => !!state.auth.me.id);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const logoutAndRedirectHome = () => {
     dispatch(logout());
-    navigate('/login');
+    navigate("/login");
   };
 
   return (
     <div>
-      <nav>
-        {isLoggedIn ? (
-          <div>
-            {/* The navbar will show these links after you log in */}
-            <Link to='/home'>Home</Link>
-            <Link to='/guide'>Guide</Link>
-            <Link to='/about'>About</Link>
-            <button type='button' onClick={logoutAndRedirectHome}>
-              Logout
-            </button>
-          </div>
-        ) : (
-          <div>
-            {/* The navbar will show these links before you log in */}
-            <Link to='/home'>Home</Link>
-            <Link to='/guide'>Guide</Link>
-            <Link to='/about'>About</Link>
-            <Link to='/login'>Login</Link>
-            <Link to='/signup'>Sign Up</Link>
-          </div>
-        )}
-      </nav>
-      <hr />
+      {isLoggedIn ? (
+        <nav>
+          {/* The navbar will show these links after you log in */}
+          <Link to="/">Home</Link>
+          <Link to="/guide">Guide</Link>
+          <Link to="/about">About</Link>
+          <button type="button" onClick={logoutAndRedirectHome}>
+            Logout
+          </button>
+        </nav>
+      ) : (
+        <nav>
+          {/* The navbar will show these links before you log in */}
+          <Link to="/">Home</Link>
+          <Link to="/guide">Guide</Link>
+          <Link to="/about">About</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Sign Up</Link>
+        </nav>
+      )}
     </div>
   );
 };

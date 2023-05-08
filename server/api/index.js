ccb09eba8a66465934fd8df04ceda023d280f909
AlexPetrovichJr <you@avidbooklearner@gmail.com>
2023-05-08 05:40:27 -0700

    completes seed, fixes routing middleware for settings and classes

--- server/api/index.js
+++ server/api/index.js
@@ -3,6 +3,8 @@ module.exports = router;
 
 router.use("/users", require("./users"));
 router.use("/generate", require("./generate"));
+router.use("/settings", require("./settings"));
+router.use("/classes", require("./classes"));
 
 router.use((req, res, next) => {
   const error = new Error("Not Found");

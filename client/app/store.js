adedbbab5a9a88d9bc957f8d63b368b67f559c04
Joseph-WhiteSWE <white.joseph.jr@gmail.com>
2023-05-08 16:01:16 -0500

    Added a class component and slice Changes to be committed: modified:   client/app/store.js new file:   client/features/class/class.js new file:   client/features/class/classSlice.js

--- client/app/store.js
+++ client/app/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
+import allClassSlice from '../../client/features/class/classSlice';
 import authReducer from '../features/auth/authSlice';
 
 const store = configureStore({
-  reducer: { auth: authReducer },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  reducer: {
+    auth: authReducer,
+    classes: allClassSlice,
+  },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
 });
 
 export default store;

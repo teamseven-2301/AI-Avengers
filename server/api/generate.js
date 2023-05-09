254cc5b1e6aabccc0b30a930289556a6f1fbe0af
Steffan Cross <steffancross@berkeley.edu>
2023-05-09 06:52:50 -0700

    chat window kinda meh styled but better than before

--- server/api/generate.js
+++ server/api/generate.js
@@ -12,7 +12,7 @@ router.post("/", async (req, res) => {
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     messages: arrayOfMessages,
-    temperature: 0.7, //this one is strange and requires messing with https://platform.openai.com/docs/api-reference/chat
+    temperature: 0.8, //this one is strange and requires messing with https://platform.openai.com/docs/api-reference/chat
     // max_tokens: 100,
   });
   res.send(completion.data.choices[0].message.content);

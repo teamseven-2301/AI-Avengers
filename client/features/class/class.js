b20b80499ff7a2a02cb97e5893c6e67c75cc048e
Steffan Cross <steffancross@berkeley.edu>
2023-05-10 12:21:35 -0700

    debugged class component, playnow now goes to /settings

--- client/features/class/class.js
+++ client/features/class/class.js
@@ -1,45 +1,44 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link, useParams } from 'react-router-dom';
-import { fetchClasses, selectClasses } from './classSlice';
-
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useParams } from "react-router-dom";
+import { fetchClasses, selectClasses } from "./classSlice";
 
 //get the class names from the classSlices object and render them to the broswer
 
 const ClassSelect = () => {
-
   const dispatch = useDispatch();
-  const classes = useSelector(state => state.classes);
+  const classes = useSelector((state) => state.classSelect);
 
   const { settingID } = useParams();
 
-  console.log("settingID is: " )
+  console.log(`settingID is: ${settingID} `);
 
   useEffect(() => {
     dispatch(fetchClasses(settingID));
   }, [dispatch]);
 
   return (
+    <div className="classesContainer">
+      <h1>{"> Choose your class"}</h1>
 
-    <div className='classesContainer'>
-    <h1>{"> Choose your class"}</h1>
+      <br></br>
 
-    <br></br>
-
-    <div className='classList'>
-      {
-        classes.map((aClass) => (
-          <Link key={aClass.id} to={`/chat`}>
-            <div className='singleClass'>
-              <section>
-                <h2> {aClass.name.toUpperCase() }</h2>
-              </section>
-            </div>
-          </Link>
-        ))
-      }
+      <div className="classList">
+        {classes ? (
+          classes.map((aClass) => (
+            <Link key={aClass.id} to={`/chat`}>
+              <div className="singleClass">
+                <section>
+                  <h2> {aClass.name.toUpperCase()}</h2>
+                </section>
+              </div>
+            </Link>
+          ))
+        ) : (
+          <p>testing</p>
+        )}
+      </div>
     </div>
-  </div>
   );
 };
 

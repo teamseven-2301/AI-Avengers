0bb7c79a4bbe31443a8efccbb0072ad718afd34c
AlexPetrovichJr <you@avidbooklearner@gmail.com>
2023-05-16 04:21:53 -0700

    sever/app.js comments out middleware line 24

--- server/app.js
+++ server/app.js
@@ -20,15 +20,15 @@ app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '..', 'public/index.
 app.use(express.static(path.join(__dirname, '..', 'public')))
 
 // any remaining requests with an extension (.js, .css, etc.) send 404
-app.use((req, res, next) => {
-  if (path.extname(req.path).length) {
-    const err = new Error('Not found')
-    err.status = 404
-    next(err)
-  } else {
-    next()
-  }
-})
+// app.use((req, res, next) => {
+//   if (path.extname(req.path).length) {
+//     const err = new Error('Not found')
+//     err.status = 404
+//     next(err)
+//   } else {
+//     next()
+//   }
+// })
 
 // sends index.html
 app.use('*', (req, res) => {

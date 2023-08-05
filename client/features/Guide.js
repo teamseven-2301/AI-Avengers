13e4f98e9ab147f089f7676b2d1817d68373a933
Steffan Cross <steffancross@berkeley.edu>
2023-08-04 18:43:48 -0700

    about page styling changes. guide page, text changes

--- client/features/Guide.js
+++ client/features/Guide.js
@@ -5,24 +5,31 @@ const Guide = () => {
     <div id="guide-container">
       <div id="guide-info">
         <h1>Unconstrained by rules or objectives, only adventure!</h1>
-        <h3>Define Your Setting & Role</h3>
+        {/* <h3>Define Your Setting & Role</h3>
         <p>
           Choose a setting and a role, AI will utilize your selection to create
           the initial prompt for a story.
-        </p>
-        <h3>Choose Your Actions</h3>
+        </p> */}
+        <h3>Actions</h3>
         <p>
-          You can decide what your character says or does. The AI will produce
-          responses from other characters, or world events for you to respond
-          to. Every adventure is unique and unexpected!
+          You decide what your character says or does. The AI will produce the
+          scenario, other characters, and world events for you to respond to.
+          Every adventure is unique and free flowing.
         </p>
-        <h3>Chatbox Messages</h3>
+        <h3>Chatbox</h3>
         <p>
-          Precise and centered responses with a single action or phrase result
-          in the best experience.
+          Specific prompts with a single action or phrase result in the best
+          experience. Try to be concise but include all the information you want
+          to get across.
         </p>
-        <h3>Explore Your Imagination</h3>
-        <p>Lastly, have fun and explore your childhood imagination!</p>
+        <h3>Imagination</h3>
+        <p>
+          You are the navigator of your story, take it wherever you see fit. You
+          can do and say almost anything and the AI will respond in turn. Of
+          course there may be some quirks but part of the adventure can be
+          pushing the boundaries as well. Lastly, have fun and explore your
+          imagination!
+        </p>
       </div>
     </div>
   );
